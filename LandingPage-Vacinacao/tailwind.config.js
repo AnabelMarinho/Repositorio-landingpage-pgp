9c24e2acbff07c6d2d22459ef02ff52c6872ecb4
AnabelMarinho <anabel.soares2001@gmail.com>
2025-07-20 14:50:55 -0300

    Adicionando animação

--- LandingPage-Vacinacao/tailwind.config.js
+++ LandingPage-Vacinacao/tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
         destaque: '#018297',
         alerta: '#D34245',
       },
+      keyframes: {
+        'fade-slide': {
+          '0%': { opacity: '0', transform: 'translateY(30px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-slide': 'fade-slide 0.8s ease-out forwards',
+      },
     },
   },
   plugins: [],
